5ea19882c06abc5b2ee3d8fef1bfa4db68a88a8d
Rubi Meneses <rubi.ventura@icloud.com>
2022-09-09 14:18:38 -0500

    first commit

---
+++ KanyeAndRonSwansonAPI/Program.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json.Linq;
+using static System.Net.WebRequestMethods;
+
+var client = new HttpClient();
+
+
+
+for (int i = 0; i < 5; i++)
+{
+    var kanyeURL = "https://api.kanye.rest/";
+
+    var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+
+    var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+
+
+
+    var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+    var ronResponse = client.GetStringAsync(ronURL).Result;
+
+    var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+
+
+    Console.WriteLine(kanyeQuote);
+    Console.WriteLine(ronQuote);
+}
