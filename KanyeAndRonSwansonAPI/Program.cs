8186a00cc6a897924270f1fb5a0cbb3ce40377ca
Rubi Meneses <rubi.ventura@icloud.com>
2022-09-11 19:47:36 -0500

    revision for loosely coupled program

--- KanyeAndRonSwansonAPI/Program.cs
+++ KanyeAndRonSwansonAPI/Program.cs
@@ -1,28 +1,13 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using KanyeAndRonSwansonAPI;
+using Newtonsoft.Json.Linq;
 using static System.Net.WebRequestMethods;
 
 var client = new HttpClient();
 
-
+var quote = new QuoteGenerator(client);
 
 for (int i = 0; i < 5; i++)
 {
-    var kanyeURL = "https://api.kanye.rest/";
-
-    var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
-
-    var kanyeQuote = JObject.Parse(kanyeResponse);
-
-
-
-
-    var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
-
-    var ronResponse = client.GetStringAsync(ronURL).Result;
-
-    var ronQuote = JArray.Parse(ronResponse);
-
-
-    Console.WriteLine($"Kanye: {kanyeQuote["quote"]}");
-    Console.WriteLine($"Ron: {ronQuote[0]}");
+    Console.WriteLine($"Kanye: {quote.KanyeQuote()}");
+    Console.WriteLine($"Ron: {quote.RonQuote()}");
 }
---
+++ KanyeAndRonSwansonAPI/QuoteGenerator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+
+namespace KanyeAndRonSwansonAPI
+{
+    public class QuoteGenerator
+    {
+        private HttpClient _client;
+
+        public QuoteGenerator(HttpClient client)
+        {
+            _client = client;
+        }
+
+        public string KanyeQuote()
+        {
+            var client = new HttpClient();
+
+            var kanyeURL = "https://api.kanye.rest/";
+
+            var kanyeResponse = _client.GetStringAsync(kanyeURL).Result;
+
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+            return kanyeQuote;
+        }
+
+        public string RonQuote()
+        {
+            var client = new HttpClient();
+
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+            var ronResponse = client.GetStringAsync(ronURL).Result;
+
+            var ronQuote = JArray.Parse(ronResponse);
+
+            return ronQuote[0].ToString();
+        }
+    }
+
+}
+
